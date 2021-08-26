6ef91c170c05b3524867807477c53f7f4bfad21c
Nicholas <nwitulski2@huskers.unl.edu>
2021-08-26 18:09:48 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nicholas Witulski
+ * Date: 2021/08/26
+ *
+ * A simple program that prints my name in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Nicholas Witulski\n");
+
+     return 0;
+ }
